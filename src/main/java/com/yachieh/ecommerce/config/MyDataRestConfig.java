80575719c6b2edf827617ab542deb4baef8b54fb
yachieh09 <yachiehhuanggg@gmail.com>
2022-06-20 19:26:56 +0800

    Create Read-Only REST APIs

---
+++ src/main/java/com/yachieh/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.yachieh.ecommerce.config;
+
+import com.yachieh.ecommerce.entity.Product;
+import com.yachieh.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable the PUT, POST, DELETE method
+        // Product API
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // productCategory API
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
